d8ce7d901144f5117ea5e9084a25f7c698097f85
krait71094 <krait71094@gmail.com>
2017-03-16 17:21:57 +0000

    Add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
    belongs_to :user
+   has_many :article_categories
+   has_many :categories, through: :article_categories
    validates :title, presence:true, length: {minimum: 3, maximum: 50} #This is line is to make sure that the title column must be filled to the database
    validates :description, presence:true, length: {minimum: 10, maximum: 300}
    validates :user_id, presence:true
